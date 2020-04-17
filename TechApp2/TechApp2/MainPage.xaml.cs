2dbd93cf2ad3e8862a953ee251d869ae1e232189
Verser <verser@outlook.com.au>
2020-04-17 10:21:46 +1000

    initial commit

---
+++ TechApp2/TechApp2/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechApp2.Views;
+using Xamarin.Forms;
+namespace TechApp2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private  void BtnLogin_Clicked(object sender, EventArgs e)
+        {
+            var MyEntry = new Entry { IsPassword = true, Placeholder = "Password" };
+
+            var username = new Entry { Placeholder = "UserID" };
+            //  Navigation.PushModalAsync(new JobList()); //working
+            Application.Current.MainPage = new NavigationPage(new JobList()); //working
+
+        }
+    }
+}
