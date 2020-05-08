216b332d31a55e6f957bec2b8f984e10daaf724f
KalyanandaVedula <bharatvedula04@gmail.com>
2020-05-08 16:33:54 +1000

    File download changes

--- TechApp2/TechApp2/Views/JobDetailed/JobDetailedJobDocuments.xaml.cs
+++ TechApp2/TechApp2/Views/JobDetailed/JobDetailedJobDocuments.xaml.cs
@@ -1,7 +1,9 @@
-﻿using Plugin.Media;
+﻿using Plugin.FilePicker;
+using Plugin.Media;
 using Plugin.Media.Abstractions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,7 +27,7 @@ namespace TechApp2.Views.JobDetailed
         {
             jobslistObject = JobService.jobDetailsModel;
              var documents = new JobDocumentViewModel();
-
+            JobDocuments.ItemsSource = jobslistObject.JobDocuments;
             //documents = jobslistObject.JobDocuments[];
 
             //byte[] bytes = Convert.FromBase64String(base64BinaryStr);
@@ -34,23 +36,30 @@ namespace TechApp2.Views.JobDetailed
         }
         private async void PickDocument_Clicked(object sender, EventArgs e)
         {
-            await CrossMedia.Current.Initialize();
-            if (!CrossMedia.Current.IsPickPhotoSupported)
-            {
-                await DisplayAlert("Warning", "NonSerializedAttribute Pic", "OK");
-                return;
+            //await CrossMedia.Current.Initialize();
+            //if (!CrossMedia.Current.IsPickPhotoSupported)
+            //{
+            //    await DisplayAlert("Warning", "NonSerializedAttribute Pic", "OK");
+            //    return;
 
+            //}
+            //_mediaFile = await CrossMedia.Current.PickPhotoAsync();
+            //if (_mediaFile == null)
+            //    return;
+
+            //LocalPathLabel.Text = _mediaFile.Path;
+
+            //FileImage.Source = ImageSource.FromStream(() =>
+            //{
+            //    return _mediaFile.GetStream();
+            //});
+
+            var file = await CrossFilePicker.Current.PickFile();
+
+            if (file != null)
+            {
+                LocalPathLabel.Text = file.FileName;
             }
-            _mediaFile = await CrossMedia.Current.PickPhotoAsync();
-            if (_mediaFile == null)
-                return;
-
-            LocalPathLabel.Text = _mediaFile.Path;
-
-            FileImage.Source = ImageSource.FromStream(() =>
-            {
-                return _mediaFile.GetStream();
-            });
 
         }
         private async void TakePhoto_Clicked(object sender, EventArgs e)
@@ -64,8 +73,8 @@ namespace TechApp2.Views.JobDetailed
             }
             _mediaFile = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
             {
-                Directory="Sample",
-                Name="MyImage.jpg"
+                Directory = "Sample",
+                Name = "MyImage.jpg"
             });
             if (_mediaFile == null)
                 return;
@@ -80,6 +89,34 @@ namespace TechApp2.Views.JobDetailed
 
         private async void UploadFile_Clicked(object sender, EventArgs e)
         {
+            //var directory = Environment.CurrentDirectory;
+            //directory = Path.Combine(directory, Environment.SystemDirectory);
+            //string file = Path.Combine(directory.ToString(), "temp.pdf");
+            //System.IO.File.WriteAllBytes(file, grnbytedata);
+
+
+            byte[] grnbytedata = jobslistObject.JobDocuments[0].FileContent;
+            var myStr = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var filePath = Path.Combine(myStr, "test.pdf");
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            string targetStr = null;
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                targetStr = "iOS";
+            }
+            else if (Device.RuntimePlatform == Device.Android)
+            {
+                targetStr = "Droid";
+            }
+            else
+            {
+                targetStr = "UWP";
+            }
+            File.WriteAllBytes(filePath, grnbytedata);
+            LocalPathLabel.Text = filePath;
+
 
         }
     }
