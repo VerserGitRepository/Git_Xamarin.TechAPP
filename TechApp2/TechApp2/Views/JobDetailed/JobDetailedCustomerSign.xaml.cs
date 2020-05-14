1e248c3d5059be582879f000d62c606a240118d3
KalyanandaVedula <bharatvedula04@gmail.com>
2020-05-14 14:18:21 +1000

    customer sin changes

--- TechApp2/TechApp2/Views/JobDetailed/JobDetailedCustomerSign.xaml.cs
+++ TechApp2/TechApp2/Views/JobDetailed/JobDetailedCustomerSign.xaml.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TechApp2.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,6 +13,7 @@ namespace TechApp2.Views.JobDetailed
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class JobDetailedCustomerSign : ContentPage
     {
+        private UpdateTechJobDto updateModel = new UpdateTechJobDto();
         public JobDetailedCustomerSign()
         {
             InitializeComponent();
@@ -25,6 +26,12 @@ namespace TechApp2.Views.JobDetailed
             Stream CustomerJObSign = await MainSignaturePad.GetImageStreamAsync(SignaturePad.Forms.SignatureImageFormat.Jpeg);
             Stream TechSignJOb = await TechSign.GetImageStreamAsync(SignaturePad.Forms.SignatureImageFormat.Jpeg);
 
+            byte[] image = new byte[CustomerJObSign.Length];
+            byte[] Techimage = new byte[TechSignJOb.Length];//declare arraysize
+            CustomerJObSign.Read(image, 0, image.Length);
+            TechSignJOb.Read(Techimage, 0, Techimage.Length);
+            updateModel.CustomerSignatureVector = image;
+            updateModel.TechnicianSignatureVector = Techimage;
 
         }
     }
